a43ce4acfdf7ef6c2ba0f79dfcacd9f6d1385c0f
hamzasenh <hamza.senhaji.11647@stu.he-ferrer.eu>
2022-12-15 16:35:42 +0100

    Initial commit

---
+++ src/js/app.js
@@ -0,0 +1,42 @@
+$(document).ready(function(){
+
+    $('.add-to-cart').on('click', function () {
+        var cart = $('.shopping-cart');
+        var imgtodrag = $(this).parents('.card').children('.item').find("img").eq(0);
+        var cartTotal = cart.attr('data-totalitems');
+        var incTotal = parseInt(cartTotal) + 1;
+        
+        if (imgtodrag) {
+            var imgclone = imgtodrag.clone()
+                .offset({
+                    top: imgtodrag.offset().top,
+                    left: imgtodrag.offset().left
+                })
+                .css({
+                    'opacity': '0.8',
+                    'position': 'absolute',
+                    'height': '150px',
+                    'width': '150px',
+                    'z-index': '100'
+                })
+                .appendTo($('body'))
+                .animate({
+                    'top': cart.offset().top + 10,
+                    'left': cart.offset().left + 10,
+                    'width': 75,
+                    'height': 75
+                }, 1000, 'easeInOutExpo');
+
+                cart.addClass('shake').attr('data-totalitems', incTotal);
+
+
+            imgclone.animate({
+                'width': 0,
+                'height': 0
+            }, function () {
+                $(this).detach()
+            });
+        }
+    });
+
+})
